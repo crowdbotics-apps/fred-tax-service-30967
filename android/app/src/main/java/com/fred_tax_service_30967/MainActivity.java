621e544b2b5c8e2ad60d3508597883d036978844
Crowdbotics <team@crowdbotics.com>
2021-09-29 19:00:19 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/fred_tax_service_30967/MainActivity.java
@@ -0,0 +1,15 @@
+package com.fred_tax_service_30967;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "fred_tax_service_30967";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "fred_tax_service_30967",
+  url: "https://fred_tax_service_30967.botics.co",
+  api: "https://fred_tax_service_30967.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
